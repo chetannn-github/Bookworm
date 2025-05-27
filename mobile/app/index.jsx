dba856297e6acd339456b85ee7b82416288c5b1a
chetannn-github <chetan.rajawat25@gmail.com>
2025-05-27 20:09:19 +0530

    feat: add RootLayout and Index components in new JSX files

---
+++ mobile/app/index.jsx
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { Image } from 'expo-image'
+
+const Index = () => {
+  return (
+    <View>
+      <Text>index</Text>
+      
+    </View>
+  )
+}
+
+export default Index
