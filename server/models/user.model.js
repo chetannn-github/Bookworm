b6adddaa24d09de5279099763efc4364395414ca
chetannn-github <chetan.rajawat25@gmail.com>
2025-05-27 09:40:32 +0530

    feat - profileImage option.

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -17,7 +17,11 @@ const userSchema = new mongoose.Schema({
     password : {
         type : String,
         required : true,
-    }
+    },
+    profileImage :{
+        type : String,
+        required : true,
+    },
 });
 
 
