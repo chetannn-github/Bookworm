f984ef672f841df945754fba5e86be5f385600af
chetannn-github <chetan.rajawat25@gmail.com>
2025-05-27 09:17:53 +0530

    feat: add user model with schema for username, email, and password

---
+++ server/models/user.model.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose"
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type : String,
+        required : true,
+        unique : true,
+        minLength : 3,
+        trim:true,
+    },
+    email : {
+        type : String,
+        required : true,
+        unique : true,
+        trim:true,
+    },
+    password : {
+        type : String,
+        required : true,
+    }
+});
+
+
+export const User = mongoose.model('User', userSchema);
+
