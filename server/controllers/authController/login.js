fc092454678d0e505e50f6a2ccbaa4b521c5029d
chetannn-github <chetan.rajawat25@gmail.com>
2025-05-28 12:45:37 +0530

    fix: improve error responses in login controller with success status

--- server/controllers/authController/login.js
+++ server/controllers/authController/login.js
@@ -7,26 +7,26 @@ export const login = async(req,res) =>{
     try {
         let {username, password} = req.body;
         if(!username  || !password) {
-            return res.json({"message" : "some fields are missing"});
+            return res.json({"message" : "some fields are missing" , "success" : false});
         }
 
         let user = await User.findOne({username});
         // console.log(user);
         if(!user) {
-            return res.json({"message" : "username or password is incorrect"});
+            return res.json({"message" : "username or password is incorrect","success" : false});
         }
 
         let isPasswordCorrect = await bcrypt.compare(password,user.password);
         
         if(!isPasswordCorrect) {
-            return res.json({"message" : "username or password is incorrect"});
+            return res.json({"message" : "username or password is incorrect","success" : false});
         }
         let token = generateToken(user.id);
         user.password = undefined;
-        return res.json({user, token});
+        return res.json({user, token,"success" : true});
 
     } catch (error) {
         console.log(error);
-        return res.json({"message" : error.message});
+        return res.json({"message" : "internal server error","success" : false});
     }
 }
