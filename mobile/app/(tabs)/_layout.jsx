2152111d81b4b3061f21b980ef951f411dfcc82d
chetannn-github <chetan.rajawat25@gmail.com>
2025-05-28 20:43:11 +0530

    feat: enhance tab layout with icons and improve screen titles for Home and Profile

--- mobile/app/(tabs)/_layout.jsx
+++ mobile/app/(tabs)/_layout.jsx
@@ -1,12 +1,49 @@
 import {Tabs} from "expo-router"
 import React from 'react'
+import {Ionicons} from "@expo/vector-icons"
+import COLORS from "../../constants/color"
+import { useSafeAreaInsets } from "react-native-safe-area-context"
 
 const TabLayout = () => {
+  let insets = useSafeAreaInsets()
   return (
-    <Tabs>
-        <Tabs.Screen name="home"/>
-        <Tabs.Screen name="profile"/>
-        <Tabs.Screen name="create"/>
+    <Tabs
+      screenOptions={{
+        headerShown : false,
+        tabBarActiveTintColor : COLORS.primary,
+        tabBarStyle : {
+          backgroundColor : COLORS.background,
+          borderTopWidth : 1,
+          borderColor : COLORS.primary,
+          paddingBottom : insets.bottom,
+          height : 60
+        }
+
+      }}
+   
+    >
+        <Tabs.Screen 
+        name="home"
+        
+        options={{
+          title : "Home",
+        
+          tabBarIcon : ({color,size})=>(<Ionicons name="home-outline" size={25} color={color}></Ionicons>)
+        }}
+        />
+        <Tabs.Screen 
+        name="create"
+        options={{
+          title : "Create",
+          tabBarIcon : ({color,size})=>(<Ionicons name="add-circle-outline" size={25} color={color}></Ionicons>)
+        }}/>
+        <Tabs.Screen
+        name="profile"
+        options={{
+          title : "Create",
+          tabBarIcon : ({color,size})=>(<Ionicons name="person-outline" size={25} color={color} ></Ionicons>)
+        }}/>
+        
     </Tabs>
   )
 }
