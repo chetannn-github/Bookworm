905ab39ac8657ad4e0a47eaf7ace47f789169d0f
chetannn-github <chetan.rajawat25@gmail.com>
2025-05-28 12:44:54 +0530

    fix: update API test route path and add CORS middleware

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 import express from "express"
 import "dotenv/config"
 import { connectToDB } from "./config/db.config.js";
-
+import cors from "cors"
 
 import authRouter from "./routes/auth.routes.js";
 import bookRouter from "./routes/book.routes.js";
@@ -11,13 +11,14 @@ import bookRouter from "./routes/book.routes.js";
 
 const app = express();
 const PORT = process.env.PORT;
-
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded());
 
 
 
-app.get("/test", (req,res)=> {
+app.get("/api/test", (req,res)=> {
+    console.log("test route hit hua hh")
     return res.json({"success" : "server is running fine"});
 })
 
@@ -26,10 +27,10 @@ app.use("/api/book",bookRouter)
 
 
 
-app.listen(PORT,async ()=> {
+app.listen(3000,async ()=> {
     try {
         await connectToDB();
-        console.log("server is running on port " + PORT)
+        console.log("server is running on port " + 3000)
     } catch (error) {
         console.log(error.message);
     }
