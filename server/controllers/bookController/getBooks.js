7b428b7ce98d0391b7cb2839e6783ed0b5b3dbab
chetannn-github <chetan.rajawat25@gmail.com>
2025-05-29 08:09:26 +0530

    fix: update pagination limit and enhance response structure in getBooks function

--- server/controllers/bookController/getBooks.js
+++ server/controllers/bookController/getBooks.js
@@ -4,21 +4,24 @@ import { Book } from "../../models/book.model.js";
 //  https://localhost:5000/api/book?page=2&limit=10
 export const getBooks = async (req,res) =>{
     try {
+        console.log("get books hit")
         let page = req.query.page || 1;
-        let limit = req.query.limit || 5;
+        let limit = req.query.limit || 20;
         let skip =( page - 1) * limit;
 
-        let books = Book.find({})
+        let books = await Book.find({})
         .sort({createdAt : -1})
         .skip(skip)
         .limit(limit)
-        .populate("user","username imageURL");
+        .populate("owner","username profileImage");
         
-        let totalBooks = Book.countDocuments();
+        let totalBooks = await Book.countDocuments();
         let totalPages = Math.ceil(totalBooks / limit);
+        console.log(books);
         
-        return res.json({books, currPage:page , totalBooks, totalPages})
+        return res.json({books, currPage:page , totalBooks, totalPages, "success" : true})
     } catch (error) {
-        
+        console.log(error)
+        return res.json({"message" : "Internal Server Error", "success" : false})
     }
 }
