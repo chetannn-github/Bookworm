e1f98cb26046ff387b9d5c02e27a0e66dd791b36
chetannn-github <chetan.rajawat25@gmail.com>
2025-05-29 08:11:45 +0530

    fix: update API_URL to correct endpoint and reorder authentication screens

--- mobile/app/(auth)/_layout.jsx
+++ mobile/app/(auth)/_layout.jsx
@@ -5,8 +5,8 @@ import { Stack } from "expo-router";
 const AuthLayout = () => {
   return (
     <Stack screenOptions={{headerShown:false}} >
-        <Stack.Screen name='signup'></Stack.Screen>
-        <Stack.Screen name='login'></Stack.Screen>
+      <Stack.Screen name='index'></Stack.Screen>
+      <Stack.Screen name='signup'></Stack.Screen>
 
     </Stack>
   )
--- mobile/constants/api.js
+++ mobile/constants/api.js
@@ -1,1 +1,1 @@
-export const API_URL = "http://192.168.0.123:3000/api/";
+export const API_URL = "http://192.168.0.101:3000/api/";
