67ee37af6b15a4dbfae6eb208379fa900ddc18c4
chetannn-github <chetan.rajawat25@gmail.com>
2025-05-29 13:42:37 +0530

    fix: refactor getBooks function to improve userID handling and streamline response structure

--- server/controllers/bookController/getBooks.js
+++ server/controllers/bookController/getBooks.js
@@ -4,22 +4,29 @@ import { Book } from "../../models/book.model.js";
 //  https://localhost:5000/api/book?page=2&limit=10
 export const getBooks = async (req,res) =>{
     try {
-        // console.log("get books hit")
+        console.log("get books hit")
         let page = req.query.page || 1;
         let limit = req.query.limit || 20;
+        let userID = req.query.userID;
         let skip =( page - 1) * limit;
-
-        let books = await Book.find({})
-        .sort({createdAt : -1})
-        .skip(skip)
-        .limit(limit)
-        .populate("owner","username profileImage");
+        let books;
+        if(userID) {
+            books = await Book.find({owner : userID}).sort({createdAt : -1});
+        }else {
+            books = await Book.find({})
+            .sort({createdAt : -1})
+            .skip(skip)
+            .limit(limit)
+            .populate("owner","username profileImage");
+            let totalBooks = await Book.countDocuments();
+            let totalPages = Math.ceil(totalBooks / limit);
+            return res.json({books, currPage:page , totalBooks, totalPages, "success" : true})
+        }
         
-        let totalBooks = await Book.countDocuments();
-        let totalPages = Math.ceil(totalBooks / limit);
-        // console.log(books);
+        return res.json({books, "success" : true})
+        console.log(books);
         
-        return res.json({books, currPage:page , totalBooks, totalPages, "success" : true})
+        
     } catch (error) {
         console.log(error)
         return res.json({"message" : "Internal Server Error", "success" : false})
